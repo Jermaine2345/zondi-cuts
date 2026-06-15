ab8ec61fb957c051d2c15145f2cdd4869af9e652
Jermaine2345 <Fale.olasubomi@gmail.com>
2026-06-15 21:07:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("form").addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    alert("Thank you! Your appointment request has been received.");
+
+});
